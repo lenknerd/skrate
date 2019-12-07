b4b5d1e0debf886528426e4c617554ad9ee7e095
David Lenkner <lenknerd@gmail.com>
2019-12-07 00:23:07 -0500

    Vars, run in background

--- persistence/run.sh
+++ persistence/run.sh
@@ -7,7 +7,4 @@ CONTAINER_NAME="skrate-persistence"
 docker build -t ${SERVER_TAG} .
 
 docker run --network bridge -p 5432:5432 -e POSTGRES_PASSWORD=postgress_password \
-	--name ${CONTAINER_NAME} ${SERVER_TAG}:latest
-
-# Kill it when done
-docker container rm ${CONTAINER_NAME}
+	--name ${CONTAINER_NAME} -d ${SERVER_TAG}:latest
