5ad5f523b8661b96988b7db2d6db6ce343a710ba
David Lenkner <lenknerd@gmail.com>
2019-12-08 15:37:07 -0500

    Added test database

--- persistence/skrate_user_setup.sh
+++ persistence/skrate_user_setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Set up appropriate user for just the skrate schema
+# Set up appropriate user for skrate database, also a testing version of database
 
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER skrate_user WITH PASSWORD 'skrate_password';
     GRANT ALL PRIVILEGES ON SCHEMA public TO skrate_user;
+    CREATE USER skrate_test_user WITH PASSWORD 'skrate_test_password';
+	CREATE DATABASE skrate_test_db;
+    GRANT ALL PRIVILEGES ON DATABASE skrate_test_db TO skrate_test_user;
+	GRANT ALL PRIVILEGES ON DATABASE skrate_test_db TO postgres;
 EOSQL
